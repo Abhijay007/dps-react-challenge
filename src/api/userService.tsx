f1057a0710baa18803d61e6ec498efa0f760848e
Abhijay007 <Abhijay007j@gmail.com>
2024-07-28 23:14:30 +0530

    (Chore): added project structure with folders for API, components, hooks, and models
    Signed-off-by: Abhijay007 <Abhijay007j@gmail.com>

---
+++ src/api/userService.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function userService() {
+	return <div>userService</div>;
+}
+
+export default userService;
---
+++ src/components/ExampleComp.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ExampleComp() {
+	return <div>ExampleComp</div>;
+}
+
+export default ExampleComp;
